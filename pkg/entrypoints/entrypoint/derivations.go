1c0bb1fdaf6f129ec2b1d79de0dd0d5558950d3c
Will Rouesnel <wrouesnel@wrouesnel.com>
2025-12-08 14:38:25 +1100

    Fix --guess-url-types parameter

--- pkg/entrypoints/entrypoint/derivations.go
+++ pkg/entrypoints/entrypoint/derivations.go
@@ -37,7 +37,7 @@ type DerivationsConfig struct {
 		//CombineEquivalents   bool     `help:"Combine substitutions which produce multiple paths onto a single space separated line"`
 		EmitTarballCacheUrls bool     `help:"If a derivation includes a hash then emit a tarball hash URL as well"`
 		TarballCacheBaseUrl  string   `default:"https://tarball.nixos.org/" help:"Tarball cache URL to generate"`
-		GuessURLTypes        bool     `help:"Modify URLs with application hints e.g. git+https - this is heuristic"`
+		GuessUrlTypes        bool     `help:"Modify URLs with application hints e.g. git+https - this is heuristic"`
 		Paths                []string `arg:"" help:"Derivation paths"`
 	} `cmd:"" help:"Recursively follow derivations and extract source input URLs"`
 }
@@ -127,7 +127,7 @@ func DerivationUrls(cmdCtx *CmdContext) error {
 				subUrls := nixconsts.SubstituteUrl(uri)
 
 				for _, subUri := range subUrls {
-					if CLI.Derivations.Urls.GuessURLTypes {
+					if CLI.Derivations.Urls.GuessUrlTypes {
 						if fetcher, found := drv.Env["fetcher"]; found {
 							if strings.HasSuffix(fetcher, "git") {
 								// Looks like a git URL
