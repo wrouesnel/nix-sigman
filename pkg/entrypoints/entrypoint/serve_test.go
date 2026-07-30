aa201476944e2776e7e579eefb2fec26c0e1996c
Will Rouesnel <wrouesnel@wrouesnel.com>
2026-07-31 00:19:29 +1000

    Framework for more benchmarks

---
+++ pkg/entrypoints/entrypoint/serve_test.go
@@ -0,0 +1,19 @@
+package entrypoint_test
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+var _ = Suite(&ServeSuite{})
+
+type ServeSuite struct {
+}
+
+//func (n *ServeSuite) SetUpSuite(c *C) {
+//	// Launch a server process
+//	entrypoint.Entrypoint()
+//}
+//
+//func BenchmarkNarFileServing(c *C) {
+//
+//}
